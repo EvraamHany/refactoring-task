b793b7ab0e1219e9775de165e482189d6a2170a9
EvraamHany <evramhany2@gmail.com>
2024-05-28 23:24:55 +0200

    - update loops with streams and conditions in MovieRentalService.java

--- src/service/MovieRentalService.java
+++ src/service/MovieRentalService.java
@@ -2,7 +2,11 @@ package service;
 
 import model.Customer;
 import model.MovieRental;
-import java.util.Objects;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import static model.MovieType.*;
 import static utils.Constants.*;
 
@@ -14,22 +18,21 @@ public class MovieRentalService {
 
 
     public String getRentalStatement(Customer customer) {
-        double totalAmount = 0;
-        int frequentEnterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n");
 
-        for (MovieRental movieRental : customer.getRentals()) {
-            double thisAmount = calculateRentalAmount(movieRental);
-            frequentEnterPoints++;
+        double totalAmount = customer.getRentals().stream()
+                .mapToDouble(this::calculateRentalAmount)
+                .sum();
 
-            if (isBonusApplicable(movieRental)) {
-                frequentEnterPoints++;
-            }
+        int frequentEnterPoints = customer.getRentals().stream()
+                .mapToInt(movieRental -> isBonusApplicable(movieRental) ? 2 : 1)
+                .sum();
 
-            result.append("\t").append(movieService.getMovies().get(movieRental.getMovieId()).getTitle()).append("\t").append(thisAmount).append("\n");
-            totalAmount += thisAmount;
-        }
+        String rentalDetails = customer.getRentals().stream()
+                .map(movieRental -> "\t" + movieService.getMovies().get(movieRental.getMovieId()).getTitle() + "\t" + calculateRentalAmount(movieRental) + "\n")
+                .collect(Collectors.joining());
 
+        result.append(rentalDetails);
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
 
@@ -37,27 +40,31 @@ public class MovieRentalService {
     }
 
     private double calculateRentalAmount(MovieRental movieRental) {
-        double thisAmount = 0;
         String code = movieService.getMovies().get(movieRental.getMovieId()).getCode();
+        Map<String, Function<MovieRental, Double>> calculationStrategies = Map.of(
+                REGULAR.getValue(), this::calculateRegularMoviesRentalAmount,
+                NEW.getValue(), this::calculateNewMoviesRentalAmount,
+                CHILDRENS.getValue(), this::calculateChildrenMoviesRentalAmount
+        );
+        return calculationStrategies.getOrDefault(code, rental -> 0.0).apply(movieRental);
+    }
 
-        if (Objects.equals(code, REGULAR.getValue())) {
-            thisAmount = getAmountBasedOnInitialMaxAndExtraCharge(movieRental,
-                    INITIAL_REGULAR_MOVIE_ABOUT,
-                    MAX_REGULAR_RENTAL_PERIOD,
-                    EXTRA_AMOUNT_PER_EACH_EXCTRA_REGULAR_MOVIE_DAY);
-        }
+    private double calculateRegularMoviesRentalAmount(MovieRental movieRental) {
+        return getAmountBasedOnInitialMaxAndExtraCharge(movieRental,
+                INITIAL_REGULAR_MOVIE_ABOUT,
+                MAX_REGULAR_RENTAL_PERIOD,
+                EXTRA_AMOUNT_PER_EACH_EXCTRA_REGULAR_MOVIE_DAY);
+    }
 
-        if (Objects.equals(code, NEW.getValue())) {
-            thisAmount = movieRental.getDays() * AMOUNT_PER_DAY_FOR_NEW_MOVIE;
-        }
+    private double calculateNewMoviesRentalAmount(MovieRental movieRental) {
+        return movieRental.getDays() * AMOUNT_PER_DAY_FOR_NEW_MOVIE;
+    }
 
-        if (Objects.equals(code, CHILDRENS.getValue())) {
-            thisAmount = getAmountBasedOnInitialMaxAndExtraCharge(movieRental,
-                    INETIAL_CHILDEREN_MOVIE_AMOUT,
-                    MAX_CHILDEREN_RENTAL_PERIOD,
-                    EXTRA_AMOUNT_PER_EACH_EXCTRA_CHILDEREN_MOVIE_DAY);
-        }
-        return thisAmount;
+    private double calculateChildrenMoviesRentalAmount(MovieRental movieRental) {
+        return getAmountBasedOnInitialMaxAndExtraCharge(movieRental,
+                INETIAL_CHILDEREN_MOVIE_AMOUT,
+                MAX_CHILDEREN_RENTAL_PERIOD,
+                EXTRA_AMOUNT_PER_EACH_EXCTRA_CHILDEREN_MOVIE_DAY);
     }
 
     private static double getAmountBasedOnInitialMaxAndExtraCharge(MovieRental movieRental,
@@ -73,6 +80,7 @@ public class MovieRentalService {
     }
 
     private boolean isBonusApplicable(MovieRental movieRental) {
-        return movieService.getMovies().get(movieRental.getMovieId()).getCode().equals(NEW.getValue()) && movieRental.getDays() > MAX_NEW_RENTAL_PERIOD;
+        return movieService.getMovies().get(movieRental.getMovieId()).getCode().equals(NEW.getValue()) &&
+                movieRental.getDays() > MAX_NEW_RENTAL_PERIOD;
     }
 }
