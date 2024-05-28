0997fd519fe617f02386137c8c2e668a7a91f36c
EvraamHany <evramhany2@gmail.com>
2024-05-28 19:25:26 +0200

    - add movie rental as a service and add pre-init of movieRentalService movies in the main method

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
+import model.Customer;
+import model.Movie;
+import model.MovieRental;
+import service.MovieRentalService;
+
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class Main {
+import static model.MovieType.*;
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+public final class Main {
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    public static void main(String[] args) {
+        MovieRentalService movieRentalService = new MovieRentalService();
 
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        //given
+        HashMap<String, Movie> movies = new HashMap<>();
+        movies.put("F001", new Movie("You've Got Mail", REGULAR.getValue()));
+        movies.put("F002", new Movie("Matrix", REGULAR.getValue()));
+        movies.put("F003", new Movie("Cars", CHILDRENS.getValue()));
+        movies.put("F004", new Movie("Fast & Furious X", NEW.getValue()));
+        movieRentalService.setMovies(movies);
+
+        //When
+        String result = movieRentalService.getRentalStatement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        //String result = movieRentalService.statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1), new MovieRental("F004", 5))));
+
+        //Then
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+        if (!result.equals(expected)) {
+            throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        }
+        System.out.println("Success");
     }
-
-    System.out.println("Success");
-  }
 }
